44bd3b4d0ed9598073d88d9150864159d4c45237
t0tem <vadimkul@gmail.com>
2017-09-11 16:59:14 +0200

    finished accuracy, started contact info

--- MyApp1/ui.R
+++ MyApp1/ui.R
@@ -29,8 +29,8 @@ shinyUI(
                       style = "font-size: 18px; font-family: Indie Flower;"),
                     h2("NextWordio", align = "center", 
                        style = "font-size: 330%; font-family: Permanent Marker; margin-top: -22px;"),
-                    p(strong(em("the app that predicts the next word as you type*")), align = "center",
-                      style = "font-family: Indie Flower;"),
+                    p(strong(em("an app that predicts the next word as you type*")), align = "center",
+                      style = "font-family: Indie Flower; font-size: 95%;"),
                     
                     textAreaInput("text", label = "", value = "", 
                                   resize="vertical", rows = 2,
@@ -55,8 +55,11 @@ shinyUI(
                         border-radius: 5px;
                         max-width: 800px;
                         margin: 0 auto;",
-                    helpText("The present App serves as a capstone project on Coursera 
-                             Data Science Specialization provided by Johns Hopkins University."),
+                    helpText("This App serves as a capstone project in Coursera 
+                             Data Science Specialization provided by Johns Hopkins University.
+                             Swiftype company, the editor of SwiftKey predictive keyboard 
+                             for Android and iOS acted as a corporate parner in this capstone, 
+                             helping to formulate the real-world problematics behind the project idea."),
                     helpText("The App works thanks to a built-in prediction algorithm 
                              based on", 
                              a("N-gram language model.", href="https://en.wikipedia.org/wiki/N-gram", target = "_blank")
@@ -83,8 +86,9 @@ shinyUI(
                     div(img(src="sbo.png", width = "100%", style = "max-width: 400px;"), style = "text-align: center;"),
                     helpText("which means that if there is no matching n-gram found in the table
                              we 'back off' to the lower order (n-1)-gram applying a coefficient 0.4 to its' score.
-                             While being quite simple to implement this technique proved to show a good
-                             accuracy on high volumes of text.")
+                             Being simplier to implement than classical techniques 
+                             (such as Kneser–Ney smoothing or Good–Turing discounting) 
+                             this technique proved to show a good accuracy on high volumes of text.")
                 )
             )
         ),
@@ -97,7 +101,26 @@ shinyUI(
                         border: 1px solid #ccc;
                         border-radius: 5px;
                         max-width: 800px;
-                        margin: 0 auto;")
+                        margin: 0 auto;",
+                    helpText("Accuracy of the algorithm has been measured with an independent ",
+                             a("benchmarking tool.", 
+                               href="https://github.com/hfoffani/dsci-benchmark", 
+                               target = "_blank"),
+                             " This tool goes through randomly selected 599 lines from blogs
+                             and 793 lines of tweets generating >28000 next word predictions each time
+                             comparing them to the real next word in text."),
+                    helpText("Results are below:"),
+                    div(img(src="Benchmark_result.png", width = "100%", style = "max-width: 500px;"), style = "text-align: center;"),
+                    br(),
+                    helpText("This means that ", strong("14.21%")," of time algorithm matches the correct
+                             word with its' top-1 prediction.", strong("22.91%")," of time one of 
+                             3 predicted words is correct. In the meantime each prediction takes only ",
+                             strong("10-11 milliseconds"), " to run (which is quite fast) and 
+                             uses only ", strong("146MB of RAM"), " on Shinyapps.io server."),
+                    helpText("This result is one the highest reported on Coursera forum by the 
+                             students of Data Science Specialization.")
+                    
+                )
             )
         ),
         tabPanel(
@@ -109,7 +132,13 @@ shinyUI(
                         border: 1px solid #ccc;
                         border-radius: 5px;
                         max-width: 800px;
-                        margin: 0 auto;")
+                        margin: 0 auto;",
+                    helpText("This app is done by"),
+                    helpText("Vadim KULAGIN. A Data Science enthusiast"),
+                    div(img(src="Photo.jpeg", width = "100%", style = "border-radius: 80px; max-width: 150px;"), style = "text-align: left;"),
+                    helpText("Any feedback and contribution is welcome on GitHub."),
+                    helpText("You can also reach out to me on LinkedIn.")
+                )
             )
         ),
         tabPanel(
